a8a4347697a6257a1773d8471df54c1ab9e83a8d
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-04-15 14:57:43 +0200

    adds register function

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let loginSectionTemplate = `
         <input id="loginUserName" type="text" placeholder="Användarnamn"><br> 
         <input id="loginPassword" type="text" placeholder="Lösenord"><br> 
         <button id="loginBtn">Logga in</button>
-        <div id="loginError"></div>
+        <div id="loginMsg"></div>
     </section>
 `;
 
@@ -17,8 +17,62 @@ let registerSectionTemplate = `
         <input id="registerUserName" type="text" placeholder="Användarnamn"><br> 
         <input id="registerPassword" type="text" placeholder="Lösenord"><br> 
         <button id="registerBtn">Registrera dig</button>
-        <div id="registerError"></div>
+        <div id="registerMsg"></div>
     </section>
 `;
 
 sectionContainer.insertAdjacentHTML("afterbegin", loginSectionTemplate + registerSectionTemplate);
+
+let registerUserName = document.getElementById("registerUserName");
+let registerPassword = document.getElementById("registerPassword");
+let registerBtn = document.getElementById("registerBtn");
+let registerMsg = document.getElementById("registerMsg");
+
+
+
+
+//registrera ny användare (identifieras genom randomiserad nyckel)
+registerBtn.addEventListener("click", function() {
+    // console.log("registerUserName.value", registerUserName.value);
+    // console.log("registerPassword.value", registerPassword.value);
+
+    let newUser = {userName: registerUserName.value, password: registerPassword.value};
+    console.log("newUser", newUser);
+
+    
+    if ( (registerUserName.value !== "") && (registerPassword.value !== "") ) {
+        console.log("fetcha");
+        fetch('http://localhost:3000/users/login', {
+
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+        .then(res => res.json())
+        .then(function(res) {
+            console.log(res);
+            if (res === "newUser saved") {
+                printRegisterSuccess();
+            }
+        });
+    } else {
+        console.log("visa error");
+        printErrorMsg();
+    }
+
+});
+
+function printErrorMsg() {
+
+    let errorMsg = `<p>Error, du måste fylla i både användarnamn och lösenord.</p>`;
+    registerMsg.innerHTML = errorMsg;
+}
+
+function printRegisterSuccess() {
+
+    let registerSuccess = `<p>Tack för din registrering. Du kan du logga in med dina nya användaruppgifter.</p>`;
+    registerMsg.innerHTML = registerSuccess;
+
+}
