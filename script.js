1dd2e7ad075bdb58836d28a7e3152188214fc646
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-04-15 15:58:44 +0200

    adds login check visible in console log

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const header = document.getElementById("header");
+// const header = document.getElementById("header");
 const sectionContainer = document.getElementById("sectionContainer");
 
 let loginSectionTemplate = `   
@@ -28,6 +28,10 @@ let registerPassword = document.getElementById("registerPassword");
 let registerBtn = document.getElementById("registerBtn");
 let registerMsg = document.getElementById("registerMsg");
 
+let loginUserName = document.getElementById("loginUserName");
+let loginPassword = document.getElementById("loginPassword");
+let loginBtn = document.getElementById("loginBtn");
+let loginMsg = document.getElementById("loginMsg");
 
 
 
@@ -36,13 +40,14 @@ registerBtn.addEventListener("click", function() {
     // console.log("registerUserName.value", registerUserName.value);
     // console.log("registerPassword.value", registerPassword.value);
 
+    loginMsg.innerHTML = ""; 
+
     let newUser = {userName: registerUserName.value, password: registerPassword.value};
     console.log("newUser", newUser);
 
-    
     if ( (registerUserName.value !== "") && (registerPassword.value !== "") ) {
         console.log("fetcha");
-        fetch('http://localhost:3000/users/login', {
+        fetch('http://localhost:3000/users/register', {
 
             method: 'post',
             headers: {
@@ -56,23 +61,85 @@ registerBtn.addEventListener("click", function() {
             if (res === "newUser saved") {
                 printRegisterSuccess();
             }
+            if (res === "userName already exists") {
+                printRegisterFail();
+            }
         });
     } else {
         console.log("visa error");
-        printErrorMsg();
+        printErrorMsg(registerMsg);
+    }
+    
+
+});
+
+loginBtn.addEventListener("click", function() {
+
+    registerMsg.innerHTML = ""; 
+    // console.log("klick loginBtn");
+
+    //borde detta vara samma som newUser i registerBtn-listener?
+    let user = {userName: loginUserName.value, password: loginPassword.value};
+    console.log(user);
+
+    if ( (loginUserName.value !== "") && (loginPassword.value !== "") ) {
+        console.log("fetcha");
+
+        fetch('http://localhost:3000/users/login', {
+
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(function(res) {
+            // console.log("res", res);
+            console.log("res.id", res.id);
+
+            if (res.id !== undefined) {
+                console.log("Login sucess - save id to lS");
+                
+                localStorage.setItem("id", res.id);
+                // printUserPage();
+            } else {
+                //Få denna div att köras
+                console.log("Login fail - show error");
+                //  errorDiv();
+            }
+
+        });
+        
+
+
+    } else {
+        console.log("visa error");
+        printErrorMsg(loginMsg);
     }
 
 });
 
-function printErrorMsg() {
+function printErrorMsg(registerMsg) {
 
     let errorMsg = `<p>Error, du måste fylla i både användarnamn och lösenord.</p>`;
     registerMsg.innerHTML = errorMsg;
-}
+};
 
 function printRegisterSuccess() {
 
-    let registerSuccess = `<p>Tack för din registrering. Du kan du logga in med dina nya användaruppgifter.</p>`;
+    let registerSuccess = `
+        <p>Tack för din registrering. Du kan du logga in med dina nya användaruppgifter.</p>
+    `;
     registerMsg.innerHTML = registerSuccess;
 
-}
+};
+
+function printRegisterFail() {
+    
+    let registerFail = `
+        <p>Användarnamnet finns redan, pröva med ett annat.</p>
+    `;
+    registerMsg.innerHTML = registerFail;
+
+};
