5d244fe64aca793b235fee265a51544826d7da01
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-04-22 20:22:22 +0200

    change back to id in url

--- script.js
+++ script.js
@@ -125,8 +125,7 @@ function printStartPage() {
                 if(res.code == "ok") {
                     // console.log("user inloggad");
                     saveToLS("id", res.userId);
-                    // printUserPage(res.userId);
-                    printUserPage();
+                    printUserPage(res.userId);
                 } else {
                     console.log("error");
                     printErrorMsg(loginMsgContainer);
@@ -150,12 +149,9 @@ function printRegisterFail() {
     registerMsgContainer.innerHTML = registerFail;
 };
 
-// function printUserPage(id) {
-    // fetch('https://vill-du-ha-mitt-nyhetsbrev-be.herokuapp.com/users/userpage/' + id )
+function printUserPage(id) {
+    fetch('https://vill-du-ha-mitt-nyhetsbrev-be.herokuapp.com/users/userpage/' + id )
 
-function printUserPage() {
-
-    fetch('https://vill-du-ha-mitt-nyhetsbrev-be.herokuapp.com/users/userpage')
     .then(data => data.json())
     .then(function(data) {
         console.log("userName + subscription", data);
@@ -195,9 +191,7 @@ function printUserPage() {
 
             let user = data;
             
-            // fetch('https://vill-du-ha-mitt-nyhetsbrev-be.herokuapp.com/users/subscribe/' + id, {
-            fetch('https://vill-du-ha-mitt-nyhetsbrev-be.herokuapp.com/users/subscribe', {
-
+            fetch('https://vill-du-ha-mitt-nyhetsbrev-be.herokuapp.com/users/subscribe/' + id, {
 
                 method: 'post',
                 headers: {
